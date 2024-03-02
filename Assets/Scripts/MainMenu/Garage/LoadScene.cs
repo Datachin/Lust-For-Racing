999c696e43d9d4e3a9a6a523261c1169d1d9d3ee
muaddibfremen <55508629+muaddibfremen@users.noreply.github.com>
2024-03-02 14:32:10 +0200

    fix load scene from mobile

--- Assets/Scripts/MainMenu/Garage/LoadScene.cs
+++ Assets/Scripts/MainMenu/Garage/LoadScene.cs
@@ -5,6 +5,10 @@ public class LoadScene : MonoBehaviour
 {
     public void LoadLevel(int _index)
     {
-        SceneManager.LoadScene(_index);
+        int currentMapIndex = PlayerPrefs.GetInt("CurrentMapIndex"); //get the index of the selected location
+        Map currentMap = ScriptableObjectsController.Instance.GetMapByIndex(currentMapIndex); // get a map of the object by index
+
+        SceneManager.LoadScene(currentMap.sceneToLoad.name);
+        
     }
 }
