89809c156c1b47101d5d21f03e6977c6497d6d94
Datachin <full.metal.shurup@gmail.com>
2024-03-11 20:55:40 +0200

    Update Replay Button

--- Assets/Scripts/Events.cs
+++ Assets/Scripts/Events.cs
@@ -1,12 +1,15 @@
-using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Events : MonoBehaviour
 {
+    private ScriptableObjectsController mapSO => ScriptableObjectsController.Instance;
 
     public void ReplayGame()
     {
-        SceneManager.LoadScene("Level_001");
+        int currentMapIndex = PlayerPrefs.GetInt("CurrentMapIndex");
+        Map currentMap = mapSO.GetMapByIndex(currentMapIndex);
+        SceneManager.LoadScene(currentMap.nameSceneToLoad);
     }
 
     public void LoadMenu()
